2212356a14111c540232ff68fb0c44596334bc2c
Alip210725 <muhamadalipmaulana3@gmail.com>
2025-05-13 00:10:31 +0700

    feat: enhance certificate preview responsiveness and structure

--- src/components/CertificatePreview.tsx
+++ src/components/CertificatePreview.tsx
@@ -3,34 +3,42 @@ import React from 'react';
 export default function CertificatePreview({ name }: { name: string }) {
     return (
         <div
-            id="certificate"
             style={{
-                width: '1123px', // A4 size in px at 96dpi
-                height: '794px',
-                margin: '0 auto',
-                position: 'relative',
-                backgroundImage: 'url("/certificate-bg.png")',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
+                width: '100%',
+                overflowX: 'auto',
+                display: 'flex',
+                justifyContent: 'center',
+                padding: '1rem 0',
             }}
         >
-            {/* Nama peserta di posisi yang sesuai dengan layout background */}
             <div
+                id="certificate"
                 style={{
-                    position: 'absolute',
-                    top: '50%', // sesuaikan
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    fontSize: '36px',
-                    fontWeight: 'bold',
-                    color: '#000000',
-                    width: '100%',
-                    textAlign: 'center',
-                    fontFamily: '"Times New Roman", serif',
+                    width: '1123px', // A4 horizontal
+                    height: '794px',
+                    position: 'relative',
+                    backgroundImage: 'url("/certificate-bg.png")',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
                 }}
             >
-                {name}
+                <div
+                    style={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        fontSize: '36px',
+                        fontWeight: 'bold',
+                        color: '#000000',
+                        width: '100%',
+                        textAlign: 'center',
+                        fontFamily: '"Times New Roman", serif',
+                    }}
+                >
+                    {name}
+                </div>
             </div>
         </div>
     );
